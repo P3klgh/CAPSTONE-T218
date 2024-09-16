7b83e57f98564cc86dd6419be3907f91893cefd1
Kate <kpkeevers20@gmail.com>
2024-09-16 19:02:31 +1000

    addBattery function completed with error handling delete kate_test.py later

--- battery.py
+++ battery.py
@@ -1,12 +1,34 @@
 from database import *
+import logging
+logging.basicConfig(level=logging.INFO)
+
 databaseName = 'maindb'
 
-def addBattery(connection, batteryName, batteryMinCapacity, batteryMaxCapacity):
-    cursor = connection.cursor()
-    sql = "INSERT INTO battery (batteryName, batteryMinCapacity, batteryMaxCapacity) VALUES (%s, %s, %s)"
-    val = (batteryName, batteryMinCapacity, batteryMaxCapacity)
-    cursor.execute(sql, val)
+def addBattery(batteryName, batteryMaxCapacity, batteryMinCapacity = 0):
+    try:
+        if batteryMaxCapacity == 0:
+            raise Exception("maxBatteryCapacity is empty")
+    except Exception as e:
+        logging.error(f"Maximum Battery Capacity cannot be empty")
+        return False
+
+    connection = databaseConnection(databaseName)
+    if testDataBaseConnection(connection) == True:
+        cursor = connection.cursor()
+        try:
+            sql = "INSERT INTO battery (batteryName, batteryMaxCapacity, batteryMinCapacity) VALUES (%s, %s, %s)"
+            val = (batteryName, batteryMaxCapacity, batteryMinCapacity)
+            cursor.execute(sql, val)
+            logging.info(cursor.rowcount, "record inserted.")
+            return True
+        except Exception as e:
+            if str(e).find('Duplicate entry') > 0:
+                logging.error(f"Battery with name '{batteryName}' already exists")
+                return False
+            else:
+                logging.error(f"An error occured: {e}")
+                return False
     cursor.close()
     connection.commit()
     closeDatabaseConnection(connection)
-    print(cursor.rowcount, "record inserted.")
+
---
+++ kate_test.py
@@ -0,0 +1,4 @@
+from database import *
+from battery import *
+
+addBattery('battery5', 0)
